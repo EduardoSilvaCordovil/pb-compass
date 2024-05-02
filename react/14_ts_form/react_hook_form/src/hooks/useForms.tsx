6a2d3120fe66f6eb2854c3ff8f43b6f5c51f85ee
Eduardo Cordovil <86244886+EduardoSilvaCordovil@users.noreply.github.com>
2024-05-02 17:44:36 -0300

    form finished

---
+++ react/14_ts_form/react_hook_form/src/hooks/useForms.tsx
@@ -0,0 +1,14 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormSchema, formSchema } from "../schemas/formSchema";
+
+export const useForms = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+  });
+  return { register, handleSubmit, errors };
+};
